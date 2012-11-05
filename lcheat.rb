14a1d7b6f582d11cfd2c77dc6176da642fb74132
sucrenoir <jm@goliwok.com>
2012-11-05 10:53:08 +0100

    small improvment

--- lcheat.rb
+++ lcheat.rb
@@ -7,23 +7,24 @@ end
 
 words = File.readlines("words_long.txt")
 
-#puts "I have #{words.size} words in my dictionnary"
-
+lp = ARGV[0].downcase
 minl = ARGV[1].to_i || 2
 must = ARGV[2] || ''
 hasl = must.downcase.split(//)
-letters = ARGV[0].downcase.split(//)
+need = hasl.size>0
+letters = lp.split(//)
 
 f = File.open('logs.txt', 'a')
 f.puts ARGV[0]
 f.close
 
+#puts "I have #{words.size} words in my dictionnary"
 #puts "Scanning best words with #{letters.size} letters : #{ARGV[0]}"
 
 words.each{|w|
 #  puts w
   lt = w.chomp.split(//)
-  nl = ARGV[0].split(//)
+  nl = lp.split(//)
   ok = true
   lt.each{|l|
     idx = nl.index(l)
@@ -37,7 +38,7 @@ words.each{|w|
 
   if ok && w.size>=minl
     res = true
-    if hasl.size>0
+    if need
       for hh in hasl
         res = false unless w.include?(hh)
       end
